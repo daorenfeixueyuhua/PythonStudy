deb191ffe4c74da3da1a1528b61e8ecb397b1875
daorenfeixueyuhua <daorenfeixueyuhua@163.com>
2018-11-23 12:18:17 +0800

    python handle database using pymysql

--- mysql-python-study/pymysql_delete.py
+++ mysql-python-study/pymysql_delete.py
@@ -1,16 +1,19 @@
-# 插入数据
-from pymysql import *
 import pymysql
+
 db = pymysql.connect('localhost', 'root', 'root', 'dada')
 cursor = db.cursor()
-sql = "insert into py_user (name) values('hello');"
+name = "update"
+id = 1
+sql = """
+    DELETE FROM py_user WHERE id = %d;
+""" % (id)
 try:
     cursor.execute(sql)
     db.commit()
-    print("插入成功")
+    print("delete successful")
 except:
-    print("插入失败")
+    print("delete error")
     db.rollback()
 
-cursor.close()
-db.close()
+finally:
+    db.close()
